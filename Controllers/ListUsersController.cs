81e74a4573015c8dfe4fb303b793ac7e4a3f2504
WetDrywall <Teisvhjelm@gmail.com>
2023-11-15 09:13:32 +0100

    Weee WOOO:D

---
+++ Controllers/ListUsersController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using GetsDoneApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
+
+namespace GetsDoneApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ListUsersController : ControllerBase
+    {
+        private readonly ListUsersContext _context;
+        public ListUsersController(ListUsersContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int uid)
+        {
+            var Sqlstr = "EXEC ListUsers @UId";
+            SqlParameter parameterS = new SqlParameter("@UId", uid);
+            var users = await _context.ListUsers.FromSqlRaw(Sqlstr, parameterS).ToListAsync();
+            return Ok(users);
+        }        
+    }
+}
