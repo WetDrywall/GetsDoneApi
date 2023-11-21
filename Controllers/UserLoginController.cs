d1352f7ea8b15c5e2d9673bff8d253634910f5d0
WetDrywall <Teisvhjelm@gmail.com>
2023-11-21 12:28:40 +0100

    Jwt token på login
    jwt

--- Controllers/UserLoginController.cs
+++ Controllers/UserLoginController.cs
@@ -4,6 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using GetsDoneApi.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace GetsDoneApi.Controllers
 {
@@ -12,9 +16,11 @@ namespace GetsDoneApi.Controllers
     public class UserLoginController : ControllerBase
     {
         private readonly UserLoginContext _context;
-        public UserLoginController(UserLoginContext context)
+        private readonly IConfiguration _configuration;
+        public UserLoginController(UserLoginContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         [HttpGet]
@@ -24,7 +30,37 @@ namespace GetsDoneApi.Controllers
             SqlParameter parameterS = new SqlParameter("@Email", email);
             SqlParameter parameterD = new SqlParameter("@Password", password);
             var users = await _context.UserLogin.FromSqlRaw(Sqlstr, parameterS, parameterD).ToListAsync();
+
+            string token = "";
+
+            if (users[0].Findes)
+            {
+                token = CreateToken(users[0].UId);
+            }
+
             return Ok(users);
+        }
+
+        private string CreateToken(int uid)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, uid.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
